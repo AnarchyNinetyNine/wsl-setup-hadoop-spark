d2556bf09284b86bec73755b82b26871ae5783ac
Idris Elgarrab <franckenstein999@gmail.com>
2025-04-23 11:02:08 +0100

    feat(correction): update the installer script | spark home dir

--- install_hadoop_spark.sh
+++ install_hadoop_spark.sh
@@ -7,7 +7,7 @@ HADOOP_HOME=$HOME/.hadoop
 SPARK_HOME=$HOME/.spark
 
 # Download and install Hadoop
-wget https://downloads.apache.org/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-src.tar.gz
 tar -xvzf hadoop-$HADOOP_VERSION.tar.gz
 mv hadoop-$HADOOP_VERSION $HADOOP_HOME
 rm hadoop-$HADOOP_VERSION.tar.gz
@@ -17,13 +17,13 @@ echo 'export HADOOP_HOME=$HOME/hadoop' >> ~/.bashrc
 echo 'export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH' >> ~/.bashrc
 
 # Download and install Spark
-wget https://downloads.apache.org/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop3.tgz
+wget https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz
 tar -xvzf spark-$SPARK_VERSION-bin-hadoop3.tgz
 mv spark-$SPARK_VERSION-bin-hadoop3 $SPARK_HOME
 rm spark-$SPARK_VERSION-bin-hadoop3.tgz
 
 # Set Spark environment variables
-echo 'export SPARK_HOME=$HOME/spark' >> ~/.bashrc
+echo 'export SPARK_HOME=$SPARK_HOME/spark-3.5.5-bin-hadoop3/' >> ~/.bashrc
 echo 'export PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 
