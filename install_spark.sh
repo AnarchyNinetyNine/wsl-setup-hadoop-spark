5cee6950a4f952b556f865a32f2338ccd801dedf
Idris Elgarrab <franckenstein999@gmail.com>
2025-04-23 21:28:31 +0100

    feat(install): Add a separate installer script for Apache Spark 3.5.5

---
+++ install_spark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Versions
+SPARK_VERSION=3.5.5
+SPARK_HOME=$HOME/.spark
+
+# Download and install Spark
+wget https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz
+tar -xvzf spark-$SPARK_VERSION-bin-hadoop3.tgz
+mv spark-$SPARK_VERSION-bin-hadoop3 $SPARK_HOME
+rm spark-$SPARK_VERSION-bin-hadoop3.tgz
+
+# Set Spark environment variables
+echo 'export SPARK_HOME=$SPARK_HOME/spark-3.5.5-bin-hadoop3/' >> ~/.bashrc
+echo 'export PATH=$SPARK_HOME/bin:$SPARK_HOME/bin:$PATH' >> ~/.bashrc
+
+# Apply changes
+source ~/.bashrc
+
+# Reload systemd and start services
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
