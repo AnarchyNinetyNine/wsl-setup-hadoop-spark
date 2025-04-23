9144290cb65e2c3986bdd84b833c9bb3896691dc
Idris Elgarrab <franckenstein999@gmail.com>
2025-04-23 23:30:49 +0100

    feat(update): update spark home's path

--- install_spark.sh
+++ install_spark.sh
@@ -2,7 +2,6 @@
 
 # Versions
 SPARK_VERSION=3.5.5
-SPARK_HOME=$HOME/.spark
 
 # Download and install Spark
 wget https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz
@@ -11,7 +10,7 @@ mv spark-$SPARK_VERSION-bin-hadoop3 $SPARK_HOME
 rm spark-$SPARK_VERSION-bin-hadoop3.tgz
 
 # Set Spark environment variables
-echo 'export SPARK_HOME=$SPARK_HOME/spark-3.5.5-bin-hadoop3/' >> ~/.bashrc
+echo 'export SPARK_HOME=$HOME/.spark/' >> ~/.bashrc
 echo 'export PATH=$SPARK_HOME/bin:$SPARK_HOME/bin:$PATH' >> ~/.bashrc
 
 # Apply changes
